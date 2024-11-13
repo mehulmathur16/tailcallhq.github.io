2573bb64a066873273e6961115f631c26305fe9a
Mehul Mathur <mathur.mehul01@gmail.com>
2024-11-13 19:17:27 +0000

    progress

--- src/components/shared/CookieConsentModal/CookieConsentModal.tsx
+++ src/components/shared/CookieConsentModal/CookieConsentModal.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect} from "react"
 import styles from "./styles.module.css"
 import clsx from "clsx"
 
@@ -28,53 +28,85 @@ const CookieConsentModal: React.FC<CookieConsentModalProps> = ({open, onAccept, 
     },
   ]
 
+  useEffect(() => {
+    if (typeof window === "undefined") return
+
+    if (open) {
+      requestAnimationFrame(() => {
+        document.body.style.overflow = "hidden"
+      })
+    } else {
+      document.body.style.overflow = "visible"
+    }
+  }, [open])
+
   return (
     <>
       {open ? (
-        <div className={clsx("p-4 bg-black", styles.cookieConsentModal)}>
-          <div className="relative p-2 border border-solid border-tailCall-border-light-600">
-            <div
-              className={clsx(
-                "flex flex-col items-center justify-center py-8 px-16 gap-5 font-space-mono border border-solid border-tailCall-border-light-600",
-                styles.bodyContainer,
-              )}
-            >
-              <div className="flex flex-col items-center justify-center p-3 gap-2 text-center">
-                <img src={require("@site/static/images/cookie-consent/cookie.png").default} height={54} width={54} />
-                <span className="text-title-medium text-tailCall-light-300">We Value Your Privacy</span>
-                <span className="text-content-small text-tailCall-light-300">
-                  Our website uses some cookies and records your IP address for the purposes of accessibility, security,
-                  and managing your access to the telecommunication network. You can disable data collection and cookies
-                  by changing your browser settings, but it may affect how this website functions, Learn more.
-                </span>
+        <>
+          {/* Overlay */}
+          <div
+            className={clsx("block fixed inset-0 bg-black bg-opacity-50", styles.modalOverlay)}
+            onClick={onClose}
+          ></div>
+
+          {/* Modal Container */}
+          <div className={clsx("p-4 bg-black", styles.cookieConsentModal)}>
+            <div className="relative p-2 border border-solid border-tailCall-border-light-600">
+              <div
+                className={clsx(
+                  "flex flex-col items-center justify-center py-8 px-16 gap-5 font-space-mono border border-solid border-tailCall-border-light-600",
+                  styles.bodyContainer,
+                )}
+              >
+                <div className="flex flex-col items-center justify-center p-3 gap-2 text-center">
+                  <img src={require("@site/static/images/cookie-consent/cookie.png").default} height={54} width={54} />
+                  <span className="text-title-medium text-tailCall-light-300">We Value Your Privacy</span>
+                  <span className="text-content-small text-tailCall-light-300">
+                    Our website uses some cookies and records your IP address for the purposes of accessibility,
+                    security, and managing your access to the telecommunication network. You can disable data collection
+                    and cookies by changing your browser settings, but it may affect how this website functions, Learn
+                    more.
+                  </span>
+                </div>
+                <div className="flex gap-6">
+                  {consentOptions.map((btn: ConsentOption, index: number) => {
+                    return (
+                      <span
+                        key={index}
+                        className={clsx(
+                          "py-1 px-3 text-title-medium bg-tailCall-dark-400 border border-solid border-tailCall-dark-300 cursor-pointer",
+                          styles.consentOption,
+                        )}
+                        onClick={() => {
+                          if (btn.onClick) btn.onClick()
+                          if (onClose) onClose()
+                        }}
+                      >
+                        {btn.text}
+                      </span>
+                    )
+                  })}
+                </div>
               </div>
-              <div className="flex gap-6">
-                {consentOptions.map((btn: ConsentOption, index: number) => {
-                  return (
-                    <span
-                      key={index}
-                      className={clsx(
-                        "py-1 px-3 text-title-medium bg-tailCall-dark-400 border border-solid border-tailCall-dark-300 cursor-pointer",
-                        styles.consentOption,
-                      )}
-                      onClick={() => {
-                        if (btn.onClick) btn.onClick()
-                        if (onClose) onClose()
-                      }}
-                    >
-                      {btn.text}
-                    </span>
-                  )
-                })}
-              </div>
+              <span
+                className={clsx(
+                  "absolute px-2 text-title-medium text-tailCall-light-300 bg-black",
+                  styles.modalHeading,
+                )}
+              >
+                Cookie Settings
+              </span>
+              <img
+                className={clsx("absolute cursor-pointer", styles.closeBtn)}
+                src={require("@site/static/images/cookie-consent/close-btn.png").default}
+                height={16}
+                width={25}
+                onClick={onClose}
+              />
             </div>
-            <span
-              className={clsx("absolute px-2 text-title-medium text-tailCall-light-300 bg-black", styles.modalHeading)}
-            >
-              Cookie Settings
-            </span>
           </div>
-        </div>
+        </>
       ) : null}
     </>
   )
