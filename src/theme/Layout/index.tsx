0903e576f77d3320478496c59a79e0b3fdb74ad3
Mehul Mathur <mathur.mehul01@gmail.com>
2024-11-16 11:58:14 +0000

    cookie consent manager hook

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,59 +1,35 @@
-import React, {useCallback, useEffect, useState} from "react"
+import React, {useEffect} from "react"
 import Layout from "@theme-original/Layout"
 import type LayoutType from "@theme/Layout"
 import type {WrapperProps} from "@docusaurus/types"
 import GlobalHead from "@site/src/components/shared/GlobalHead"
 import CookieConsentModal from "@site/src/components/shared/CookieConsentModal/CookieConsentModal"
-import {useCookieConsent} from "@site/src/utils/hooks/useCookieConsent"
+import {useCookieConsentManager} from "@site/src/utils/hooks/useCookieConsentManager"
 
 type Props = WrapperProps<typeof LayoutType>
 
 export default function LayoutWrapper(props: Props): JSX.Element {
-  const {getCookieConsent, setCookieConsent} = useCookieConsent()
-  const [showCookieConsentModal, setShowCookieConsentModal] = useState(false)
-  const cookieConsent = getCookieConsent()
+  const {
+    showCookieConsentModal,
+    openCookieConsentModal,
+    closeCookieConsentModal,
+    onAccept,
+    onDeny,
+    onPartialAccept,
+    cookieConsent,
+  } = useCookieConsentManager()
 
-  // Check for existing consent in cookies on initial render
   useEffect(() => {
     if (!cookieConsent) {
-      // Show modal if no consent cookie exists
-      setShowCookieConsentModal(true)
+      openCookieConsentModal()
     }
   }, [cookieConsent])
-
-  const handleCookieConsentModalClose = useCallback(() => {
-    setShowCookieConsentModal(false)
-  }, [])
-
-  const onAccept = useCallback(() => {
-    const consentData = {accepted: true}
-    setCookieConsent(consentData)
-    handleCookieConsentModalClose()
-  }, [setCookieConsent])
-
-  const onDeny = useCallback(() => {
-    const consentData = {accepted: false}
-    setCookieConsent(consentData)
-    handleCookieConsentModalClose()
-  }, [setCookieConsent])
-
-  const onPartialAccept = useCallback(
-    (selectedPreferences: string[]) => {
-      const consentData = {
-        accepted: true,
-        preferences: selectedPreferences,
-      }
-      setCookieConsent(consentData)
-      handleCookieConsentModalClose()
-    },
-    [setCookieConsent],
-  )
 
   return (
     <>
       <CookieConsentModal
         open={showCookieConsentModal}
-        onClose={handleCookieConsentModalClose}
+        onClose={closeCookieConsentModal}
         onAccept={onAccept}
         onDeny={onDeny}
         onPartialAccept={onPartialAccept}
