01358e097597d3cb7337415cbc9ca310a2f404f7
Tushar Mathur <tusharmath@gmail.com>
2024-12-14 13:54:07 -0800

    update banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -18,7 +18,7 @@ const Banner = (): JSX.Element => {
             as="h1"
             className="hero-banner-title text-title-large max-w-xs sm:text-display-small lg:text-display-large sm:max-w-5xl"
           >
-            AI powered <br />
+            The modern <br />
             <span className="bg-tailCall-yellow rounded-md sm:rounded-2xl px-SPACE_02">GraphQL</span> platform
           </Heading>
           <p className="hero-banner-sub-title sm:max-w-2xl sm:m-auto text-content-small sm:text-content-medium lg:text-content-large font-normal max-w-md sm:mt-SPACE_04 mb-0">
